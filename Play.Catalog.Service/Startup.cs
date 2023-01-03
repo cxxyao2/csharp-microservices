53b1c24445e6c7467a25fa088907d769ff5a6a2a
jennifer <cxxyao2@gmail.com>
2023-01-02 19:56:29 -0500

    before using lib

--- Play.Catalog.Service/Startup.cs
+++ Play.Catalog.Service/Startup.cs
@@ -34,24 +34,11 @@ namespace Play.Catalog.Service
     public void ConfigureServices(IServiceCollection services)
     {
 
-      BsonSerializer.RegisterSerializer(new GuidSerializer(MongoDB.Bson.BsonType.String));
-      // Everytime we save a DateTimeOffset value, it will be saved as a string in MongoDB
-      BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(MongoDB.Bson.BsonType.String));
-
       serviceSettings = Configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
 
-      services.AddSingleton(serviceProvider =>
-      {
-        var mongoDbSettings = Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
-        var mongoClient = new MongoClient(mongoDbSettings.ConnectionString);
-        return mongoClient.GetDatabase(serviceSettings.ServiceName);
-      });
+      services.AddMongo()
+      .AddMongoRepository<Item>("items");
 
-      services.AddSingleton<IRepository<Item>>(serviceProvider =>
-      {
-        var database = serviceProvider.GetService<IMongoDatabase>();
-        return new MongoRepository<Item>(database, "items");
-      });
 
       services.AddControllers(options =>
       {
