191adcd8acc6909c5d725cfb1165ef26c2885ce6
jennifer <cxxyao2@gmail.com>
2023-01-05 18:46:57 -0500

    add CORS to service

--- Play.Catalog.Service/Startup.cs
+++ Play.Catalog.Service/Startup.cs
@@ -65,11 +65,19 @@ namespace Play.Catalog.Service
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
+      var AllowedOriginSetting = "";
       if (env.IsDevelopment())
       {
         app.UseDeveloperExceptionPage();
         app.UseSwagger();
         app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Play.Catalog.Service v1"));
+        app.UseCors(builder =>
+        {
+          builder.WithOrigins(Configuration[AllowedOriginSetting])
+           .AllowAnyHeader()
+          .AllowAnyMethod();
+        });
+
       }
 
       app.UseHttpsRedirection();
